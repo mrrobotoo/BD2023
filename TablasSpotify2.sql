0abf5851825c88fc25f8eb41ecb6512b09e43fe2
chokoo7 <adanzamudio777@gmail.com>
2023-12-12 08:04:07 -0600

    INSERT DE TABLAS

---
+++ TablasSpotify2.sql
@@ -0,0 +1,235 @@
+/* Artistas*/
+
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Isabella Mariposa',3000,5);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Xavier Luna',20000,10);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Violeta Mirage',500,0);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Leonardo Vortex',59866,8);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Serena Sky',9999,7);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Dominic Echo',898750,28);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Luna Solstice',48150,26);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('IEvangeline Muse',7412,5);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Orion Storm',3000,1);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Isidro Velvet',9874,5);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Electra Nova',9368,7);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Dante Stardust',36279,15);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Calista Phoenix',3000,7);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Atlas Aurora',3206,8);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Sirena Celeste',986,10);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Zenith Blaze',369,5);
+INSERT INTO ARTIST(Nombre_Artista,Num_Seguidores,Top_Canciones) VALUES('Billie Eilish',10000,10);
+
+/*Gneero*/
+INSERT INTO Generos(Tipo_genero) values('Tango');
+INSERT INTO Generos(Tipo_genero) values('Pop');
+INSERT INTO Generos(Tipo_genero) values('Electrónica');
+INSERT INTO Generos(Tipo_genero) values('Jazz');
+INSERT INTO Generos(Tipo_genero) values('Salsa');
+INSERT INTO Generos(Tipo_genero) values('Reggae');
+INSERT INTO Generos(Tipo_genero) values('Clásico');
+INSERT INTO Generos(Tipo_genero) values('Acústico');
+INSERT INTO Generos(Tipo_genero) values('Tribal');
+INSERT INTO Generos(Tipo_genero) values('Space Rock');
+INSERT INTO Generos(Tipo_genero) values('Blues');
+INSERT INTO Generos(Tipo_genero) values('Calypso');
+INSERT INTO Generos(Tipo_genero) values('Afrobeat');
+INSERT INTO Generos(Tipo_genero) values('Electro swing');
+INSERT INTO Generos(Tipo_genero) values('Bollywood');
+INSERT INTO Generos(Tipo_genero) values('Nocturno');
+INSERT INTO Generos(Tipo_genero) values('Cumbia');
+INSERT INTO Generos(Tipo_genero) values('Trance');
+INSERT INTO Generos(Tipo_genero) values('New Age');
+INSERT INTO Generos(Tipo_genero) values('Caribbean');
+INSERT INTO Generos(Tipo_genero) values('Desert Symphony');
+INSERT INTO Generos(Tipo_genero) values('Latin');
+INSERT INTO Generos(Tipo_genero) values('Dub Reggae');
+INSERT INTO Generos(Tipo_genero) values('Tropical');
+INSERT INTO Generos(Tipo_genero) values('Folk');
+INSERT INTO Generos(Tipo_genero) values('Desert Song');
+INSERT INTO Generos(Tipo_genero) values('Twilight Classical');
+INSERT INTO Generos(Tipo_genero) values('Bollywood Sunrise');
+INSERT INTO Generos(Tipo_genero) values('Forest Reggae');
+INSERT INTO Generos(Tipo_genero) values('Tropical Rhythm');
+INSERT INTO Generos(Tipo_genero) values('Epic pop');
+INSERT INTO Generos(Tipo_genero) values('Deep Space Melody');
+INSERT INTO Generos(Tipo_genero) values('Electronic Adventure');
+INSERT INTO Generos(Tipo_genero) values('Beach Rock');
+INSERT INTO Generos(Tipo_genero) values('Caribbean Flavor');
+INSERT INTO Generos(Tipo_genero) values('Classical Arpeggios');
+INSERT INTO Generos(Tipo_genero) values('Jungle Beat');
+INSERT INTO Generos(Tipo_genero) values('Funk');
+INSERT INTO Generos(Tipo_genero) values('Acoustic Dawn');
+INSERT INTO Generos(Tipo_genero) values('Dub Reggae');
+INSERT INTO Generos(Tipo_genero) values('Desert Serenade');
+INSERT INTO Generos(Tipo_genero) values('Cosmic Rock');
+INSERT INTO Generos(Tipo_genero) values('Flamenco');
+INSERT INTO Generos(Tipo_genero) values('Contemporaneo');
+INSERT INTO Generos(Tipo_genero) values('Neo-Clasico');
+INSERT INTO Generos(Tipo_genero) values('Futuristic Rock');
+INSERT INTO Generos(Tipo_genero) values('Desert Blues');
+INSERT INTO Generos(Tipo_genero) values('Tribal');
+INSERT INTO Generos(Tipo_genero) values('Electronic Harmony');
+INSERT INTO Generos(Tipo_genero) values('Electronic Atmosphere');
+INSERT INTO Generos(Tipo_genero) values('Modern Tango');
+
+
+/*USUARIO*/
+
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Olivia Johnson',' user1@example.com','pass123',1);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Ethan Miller','user2@example.com','abc456',2);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Sophia Davis','testuser@email.com','qwerty789',3);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Jackson Turner',' demo@gmail.com','demoPass',4);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Ava Mitchell',' fakeuser@hotmail.com','mySecret123',5);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Liam Carter',' exampleuser@yahoo.com','testpass456',6);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Isabella Walker',' testing123@outlook.com','passTest789',7);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Noah Bennett','userdemo@mail.com','demo1234',8);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Emma Parker','tempuser@domain.com','securePwd789',9);
+INSERT INTO Usuario(nombre_usuario,email,contra,seguidores) VALUES('Lucas Taylor',' mytestuser@gmail.com','pass123abc',10);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Olivia Johnson','oliviajohnson@gmail.com','12345',486);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Ethan Miller','ethanmiller@gmail.com','abc123',984);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Sophia Davis','sophiadavis@gmail.com','DummyPwd456',98);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Jackson Turner','jacksonturner@gmail.com','SafePassword123',963);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Ava Mitchell','avamitchell@gmail.com','UniquePass789',963);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Liam Carter','liamcarter@gmail.com','TestPwd!456#',123);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Isabella Walker','isabellawalker@gmail.com','Access123Key',78);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Noah Bennett','noahbennett@gmail.com','Secure!Pass890',1);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Emma Parker','emmaparker@gmail.com','DemoPassword789',963);
+INSERT INTO Usuario(nombre_usuario, email, contra, seguidores) VALUES ('Lucas Taylor','lucastaylor@gmail.com','StrongPwd456',0);
+
+/*SONGS*/
+
+insert into Songs (Nombre_Cancion, Duracion) values ('Roots Reggae', 4.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('El Susurro del Bosque', 5.50);
+insert into Songs (Nombre_Cancion, Duracion) values ('Viaje al Caribe', 4.15);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ecos Clasicos', 4.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ballet Pop', 4.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Sinfonia del Sahara', 5.55);
+insert into Songs (Nombre_Cancion, Duracion) values ('Folk del Norte', 4.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Estrellas del Rock', 6.10);
+insert into Songs (Nombre_Cancion, Duracion) values ('Armonia Electronica', 5.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo Latino', 4.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Aurora Acustica', 3.50);
+insert into Songs (Nombre_Cancion, Duracion) values ('Extasis Bollywoos', 5.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Dub Reggae', 4.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Viaje al Espacio Interior', 7.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo Tropical', 4.15);
+insert into Songs (Nombre_Cancion, Duracion) values ('Concierto Clasico', 4.25);
+insert into Songs (Nombre_Cancion, Duracion) values ('Melodia Pop', 4.10);
+insert into Songs (Nombre_Cancion, Duracion) values ('Serenata del Desierto', 5.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('Rock Cosmico', 6.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Fusion Folklorica', 4.35);
+insert into Songs (Nombre_Cancion, Duracion) values ('Sinfonia de la Ciudad', 6.40);
+insert into Songs (Nombre_Cancion, Duracion) values ('Tango Nocturno', 4.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Noche de Blues', 3.45);
+insert into Songs (Nombre_Cancion, Duracion) values ('Electro Funk', 5.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Flamenco de la Montaña', 4.10);
+insert into Songs (Nombre_Cancion, Duracion) values ('Roots Reggae', 420);
+insert into Songs (Nombre_Cancion, Duracion) values ('El Susurro del Bosque', 550);
+insert into Songs (Nombre_Cancion, Duracion) values ('Viaje al Caribe', 415);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ecos Clasicos', 400);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ballet Pop', 405);
+insert into Songs (Nombre_Cancion, Duracion) values ('Sinfonia del Sahara', 555);
+insert into Songs (Nombre_Cancion, Duracion) values ('Folk del Norte', 430);
+insert into Songs (Nombre_Cancion, Duracion) values ('Estrellas del Rock', 610);
+insert into Songs (Nombre_Cancion, Duracion) values ('Armonia Electronica', 520);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo Latino', 405);
+insert into Songs (Nombre_Cancion, Duracion) values ('Aurora Acustica', 350);
+insert into Songs (Nombre_Cancion, Duracion) values ('Extasis Bollywoos', 505);
+insert into Songs (Nombre_Cancion, Duracion) values ('Dub Reggae', 430);
+insert into Songs (Nombre_Cancion, Duracion) values ('Viaje al Espacio Interior', 700);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo Tropical', 415);
+insert into Songs (Nombre_Cancion, Duracion) values ('Concierto Clasico', 425);
+insert into Songs (Nombre_Cancio, Duracion) values ('Melodia Pop', 410);
+insert into Songs (Nombre_Cancion, Duracion) values ('Serenata del Desierto', 520);
+insert into Songs (Nombre_Cancion, Duracion) values ('Rock Cosmico', 600);
+insert into Songs (Nombre_Cancion, Duracion) values ('Fusion Folklorica', 435);
+insert into Songs (Nombre_Cancion, Duracion) values ('Sinfonia de la Ciudad', 640);
+insert into Songs (Nombre_Cancion, Duracion) values ('Tango Nocturno', 430);
+insert into Songs (Nombre_Cancion, Duracion) values ('Noche de Blues', 345);
+insert into Songs (Nombre_Cancion, Duracion) values ('Electro Funk', 500);
+insert into Songs (Nombre_Cancion, Duracion) values ('Flamenco de la Montaña', 410);
+insert into Songs (Nombre_Cancion, Duracion) values ('Roots Reggae', 4.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('El Susurro del Bosque', 5.50);
+insert into Songs (Nombre_Cancion, Duracion) values ('Viaje al Caribe', 4.15);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ecos Clasicos', 4.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ballet Pop', 4.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Sinfonia del Sahara', 5.55);
+insert into Songs (Nombre_Cancion, Duracion) values ('Folk del Norte', 4.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Estrellas del Rock', 6.10);
+insert into Songs (Nombre_Cancion, Duracion) values ('Armonia Electronica', 5.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo Latino', 4.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Aurora Acustica', 3.50);
+insert into Songs (Nombre_Cancion, Duracion) values ('Extasis Bollywoos', 5.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Dub Reggae', 4.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Viaje al Espacio Interior', 7.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo Tropical', 4.15);
+insert into Songs (Nombre_Cancion, Duracion) values ('Concierto Clasico', 4.25);
+insert into Songs (Nombre_Cancion, Duracion) values ('Melodia Pop', 4.10);
+insert into Songs (Nombre_Cancion, Duracion) values ('Serenata del Desierto', 5.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('Rock Cosmico', 6.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Fusion Folklorica', 4.35);
+insert into Songs (Nombre_Cancion, Duracion) values ('Sinfonia de la Ciudad', 6.40);
+insert into Songs (Nombre_Cancion, Duracion) values ('Tango Nocturno', 4.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Noche de Blues', 3.45);
+insert into Songs (Nombre_Cancion, Duracion) values ('Electro Funk', 5.00);
+insert into Songs (Nombre_Cancion, Duracion) values ('Flamenco de la Montaña', 4.10);
+insert into Songs (Nombre_Cancion, Duracion) values ('Jazz en el Parque', 6.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('Atmosfera Electronica', 5.10);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo de la selva', 4.25);
+insert into Songs (Nombre_Cancion, Duracion) values ('Sabor Caribe', 4.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Arpegios Clasicos', 4.15);
+insert into Songs (Nombre_Cancion, Duracion) values ('Pop del Futuro', 4.20);
+insert into Songs (Nombre_Cancion, Duracion) values ('Canciones del Desierto', 5.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Rock en la Playa', 6.05);
+insert into Songs (Nombre_Cancion, Duracion) values ('Fusion Espacial', 5.40);
+insert into Songs (Nombre_Cancion, Duracion) values ('Ritmo de Media Noche', 4.15);
+insert into Songs (Nombre_Cancion, Duracion) values ('Tango Moderno', 4.40);
+insert into Songs (Nombre_Cancion, Duracion) values ('Blues del Amanecer', 3.50);
+insert into Songs (Nombre_Cancion, Duracion) values ('CopyCat', 3.17);
+insert into Songs (Nombre_Cancion, Duracion) values ('Dream On', 4.28);
+insert into Songs (Nombre_Cancion, Duracion) values ('Francis Forever', 2.29);
+insert into Songs (Nombre_Cancion, Duracion) values ('Hot Sauce', 3.15);
+insert into Songs (Nombre_Cancion, Duracion) values ('Instant Crush', 5.39);
+insert into Songs (Nombre_Cancion, Duracion) values ('Keep Myself Alive', 3.36);
+insert into Songs (Nombre_Cancion, Duracion) values ('Lucifer', 3.54);
+insert into Songs (Nombre_Cancion, Duracion) values ('No Surprise', 3.49);
+insert into Songs (Nombre_Cancion, Duracion) values ('Smells Like Teen', 5.01);
+insert into Songs (Nombre_Cancion, Duracion) values ('2 Baddies', 3.51);
+insert into Songs (Nombre_Cancion, Duracion) values ('19-2000', 3.30);
+insert into Songs (Nombre_Cancion, Duracion) values ('Alien Blue', 2.36);
+insert into Songs (Nombre_Cancion, Duracion) values ('Bag Guy', 3.14);
+insert into Songs (Nombre_Cancion, Duracion) values ('Rosa Pastel', 3.07);
+
+/*ALBUM*/
+
+
+
+/*PLAYLIST*/
+
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Don goyo','Privada',72);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Canciones del Recuerdo','Privada',72);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Fiesta Nocturna','Publica',24);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Sesión Relajante','Publica',60);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Atardecer Musical','Privada',48);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Aventura Sonora','Publica',72);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Historias de Amor','Publica',27);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Sintonía de Amor','Privada',24);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Momentos Románticos','Publica',48);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Dance Party Mix','Privada',64);
+INSERT INTO PLAYLIST(Nombre_Playslist,Privacidad_Playlist,Duracion_Playslist) VALUES('Noche de Fiesta','Privada',10);
+
+/*Playslist_Usuarios*/
+
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Musica relajante','Publico',56,1,1);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Musica para entrenar','Privada',80,4,8);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Corridos tumbados','Publico',78,6,3;
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Musica de los 90','Privada',63,10,2);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Fav 9.0','Publica',93,10,4);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Musica Epica','Privada',35,9,8);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Fav 2.0','Publica',105,11,5);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Ft3873.0','Privada',41,7,6);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Clasica de los 80','Publica',120,12,10);
+INSERT INTO Playlist_Usuarios(Nombre_Playlist,Privacidad_Playlist,Duracion_Playlist,id_Songs,id_Usua) values ('Mamacita 2.0','Privada',71,17,7);
+
+/*SUSCRIPCIONES*/
+
+INSERT INTO Suscripcion(Tipo_Sub) VALUES('');
